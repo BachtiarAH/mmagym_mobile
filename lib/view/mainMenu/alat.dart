cb4db6537f3f83411eea0d80a64f2b5cd6ba44b9
BachtiarAH <bachtiarah73@gmail.com>
2022-12-25 22:45:52 +0700

    tes

--- lib/view/mainMenu/alat.dart
+++ lib/view/mainMenu/alat.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:mmagym_mobile/clien/AlatClient.dart';
 import 'package:mmagym_mobile/models/AlatModel.dart';
+import 'package:mmagym_mobile/view/isimenulatihan2/isimenulatihan.dart';
 
 class AlatView extends StatefulWidget {
   const AlatView({super.key});
@@ -41,27 +42,32 @@ class _AlatState extends State<AlatView> {
                     crossAxisSpacing: 20,
                     mainAxisSpacing: 20),
                 itemBuilder: (context, index) {
-                  return GridTile(
-                    footer: GridTileBar(
-                      title: Text(
-                        snapshot.data!.body![index].nama,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20,
+                  return InkWell(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => isimenulatihan(),));
+                    },
+                    child: GridTile(
+                      footer: GridTileBar(
+                        title: Text(
+                          snapshot.data!.body![index].nama,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                          ),
                         ),
+                        // subtitle: Text(_item.description),
+                        backgroundColor: Colors.black38,
                       ),
-                      // subtitle: Text(_item.description),
-                      backgroundColor: Colors.black38,
-                    ),
-                    child: GestureDetector(
-                      // onTap: () => _selectItem(context),
-                      child: Hero(
-                        key: Key(
-                            'https://drive.google.com/uc?export=view&id=${snapshot.data!.body![index].gambar}'),
-                        tag: snapshot.data!.body![index].nama,
-                        child: Image.network(
-                          'https://drive.google.com/uc?export=view&id=${snapshot.data!.body![index].gambar}',
-                          fit: BoxFit.cover,
+                      child: GestureDetector(
+                        // onTap: () => _selectItem(context),
+                        child: Hero(
+                          key: Key(
+                              'https://drive.google.com/uc?export=view&id=${snapshot.data!.body![index].gambar}'),
+                          tag: snapshot.data!.body![index].nama,
+                          child: Image.network(
+                            'https://drive.google.com/uc?export=view&id=${snapshot.data!.body![index].gambar}',
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                     ),
